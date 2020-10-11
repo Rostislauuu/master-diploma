4a881ecc30783c0669fb927b5bbce091bfdf6d26
Pavlo <r1d3rreality@gmail.com>
2020-10-11 22:07:48 +0200

    Revert "fix text app.js"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 const App = () => {
   return (
     <div className="App">
-      <p>Diploma project </p>
+      Diploma projecttt
     </div>
   );
 }
