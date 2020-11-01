b0518b5daa900b8e67bb757bd1274d10fb2f104d
R1d3Reality <r1d3rreality@gmail.com>
2020-11-01 10:45:44 +0100

    fix header style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,20 +5,20 @@ import avatar from "../../assets/avatar.png";
 
 const Header = () => {
     return (
-            <header className="header">
-                <section className="header_container">
-                    <h1 className="logo">
-                        <a href="#">Study<span>Max</span></a>
-                    </h1>
-                    <nav className="menu_effect">
-                        <a href="#">Головна</a>
-                        <a href="#">Тести</a>
-                        <a href="#">Завдання</a>
-                        <a href="#">Групи</a>
-                        <img src={avatar} alt="Avatar" className="avatar" />
-                    </nav>
-                </section>
-            </header>
+        <header className="header">
+            <section className="header_container">
+                <h1 className="logo">
+                    <a href="#">Study<span>Max</span></a>
+                </h1>
+                <nav className="menu_effect">
+                    <a href="#">Головна</a>
+                    <a href="#">Тести</a>
+                    <a href="#">Завдання</a>
+                    <a href="#">Групи</a>
+                </nav>
+                <img src={avatar} alt="Avatar" className="avatar"/>
+            </section>
+        </header>
     );
 };
 
